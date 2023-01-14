af10b8c25e1144a1e4f96a4608cec8508324f5fb
LABORATORIO <gomezpradoroybrayam@gmail.com>
2023-01-14 09:17:48 -0500

    "Estructura MiAmigoArtificial"

--- ClasePrincipal/MiAmigoArtificial.py
+++ ClasePrincipal/MiAmigoArtificial.py
@@ -1,8 +1,50 @@
-from LABORATORIO.MiAmigoArtificial.Presentacion.Presentacion import saludar, adivinar_edad
+from LABORATORIO.MiAmigoArtificial.Presentacion.Presentacion import saludar, adivinar_edad, despedida
 
+def recordarNombre():
+    nombre = input("\nBueno ahora es tu turno de decirme como te llamas: ").upper()
+
+
+    print(f"\nQue fabuloso nombre tienes {nombre}"
+          "\ny sabes, seré tu mejor amigo o mejor dicho seremos grandes amigos!!"
+          "\nTe acompañaré por mil años asi que necesito que me cuides si quieres"
+          "\nque te acompañe por estas hermosas aventuras")
+
+def HacerEleccion():
+
+    while True:
+        print("\nHey! que te gustaria jugar comnigo")
+        print("1. ALERTA")
+        print("2. AYUDA")
+        print("3. CHISTES")
+        print("4. HISTORIAS")
+        print("5. RECOMENDACIONES")
+        print("6. salir")
+        opcion_de_entrada = int(input())
+        if opcion_de_entrada == 1:
+            #OntenerCumpleaños()
+            break
+        elif opcion_de_entrada == 2:
+            #ObtenerAyuda()
+            break
+        elif opcion_de_entrada == 3:
+            #ObtenerChistes()
+            break
+        elif opcion_de_entrada == 4:
+            #ObtenerHistorias()
+            break
+        elif opcion_de_entrada == 5:
+            #ObtenerRecomendaciones()
+            break
+        elif opcion_de_entrada == 6:
+            despedida()
+            break
+        else:
+            print("OPCION INCORRECTA!")
 
 def MiAmigoArtificial():
     saludar()
     adivinar_edad()
+    recordarNombre()
+    HacerEleccion()
 
 MiAmigoArtificial()
--- Presentacion/Presentacion.py
+++ Presentacion/Presentacion.py
@@ -1,12 +1,11 @@
 def saludar():
     print("\n\tPROJETCS: MI AMIGO ARTIFICIAL")
-    print("""
-               Fui creado el año 1969 fecha en la que el hombre dio un salto 
-               Pero no de un salto, sino un salto de la humanidad del hombre vale""")
+    print("\nFui creado el año 1969 fecha en la que el hombre dio un salto"
+          "\nPero no de un salto, sino un salto de la humanidad del hombre vale.")
 
 def adivinar_edad():
-    print("\nMira te digo, Que soy increible adivinando tu edad y apuesto que lo haré")
-    print("Asi que comenzamos")
+    print("\nMira te digo, Que soy increible adivinando tu edad "
+          "\ny apuesto que lo haré, asi que comenzamos")
     print("Cúal es el resto al dividir tu edad por 3: ")
     resto3 = int(input())
     print("Ahora cúal es el resto al dividir tu edad por 5: ")
@@ -14,6 +13,18 @@ def adivinar_edad():
     print("Finalmente cúal es el resto al dividir tu edad por 7: ")
     resto7 = int(input())
     edad = (resto3 * 70 + resto5 * 21 + resto7 * 15) % 105
-    print("haciendo un esfuerzo mental y mis fabulosos cálculos")
-    print(f"Me arroja que tu edad es {edad} y de hecho somos compatibles asi como")
-    print("el signo nega y posi")
+    print(f"\nhaciendo un esfuerzo mental y mis fabulosos cálculos"
+          f"\nMe arroja que tu edad es {edad} y de hecho somos compatibles asi como"
+          f"\nel signo nega y posi")
+
+def despedida():
+    respuesta1 = input("\nYa te vas, tan pronto?: si/no").upper()
+    if respuesta1 == "SI":
+        print("\nQue tengas un lindo día, cuidate mucho"
+              "\nmi querido amigo, me encanto ser tu amigo "
+              "\nen estos mil años de aventura <3")
+    else:
+        respuesta2 = input("Desea ver un lindo gatito? si/no").upper()
+        if respuesta2 == "SI":
+            print("Gatito")
+            breakpoint()
